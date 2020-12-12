6744a6014dd7d633fc35115ea7b4fcd9fd9e2f25
aubrey-heim <aubrey.l.heim@gmail.com>
2020-12-12 13:29:28 -0800

    added vote model

---
+++ models/vote.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+  var Vote = sequelize.define("Vote", {
+    gif: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    jelly: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    }
+  });
+
+  Vote.associate = function(models) {
+    Vote.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  Vote.associate = function(models) {
+    Vote.belongsTo(models.GiphyPost, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Vote;
+};
