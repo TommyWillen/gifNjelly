53fb6bd32787897e1d683fe2652295bab0ea5713
Tommy Willen <tommyallen1215@gmail.com>
2020-12-15 17:58:29 -0800

    start over because had some install issues

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-use gifNjelly;
-
-INSERT INTO users(firstName, lastName, userName, email, password) VALUES ("Testy", "McTesterson", "Test", "user@test.com", "testpassword0");
-INSERT INTO giphyPosts (gifId, caption, gifScore, jellyScore, userId) VALUES ("YsTs5ltWtEhnq", "When John talks about Travis", 1, 0, 1);
-INSERT INTO votes (gif, jelly, userId, giphyPostId) VALUES (true, false, 1, 1);
