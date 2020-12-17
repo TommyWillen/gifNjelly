3b3798253788f8658d68e5fce2604d27ba20d695
CarolY <mon_alisa75@yahoo.com>
2020-12-17 10:54:34 -0800

    rewrite post to show selected url, handlebars still not reading post.

--- public/js/members.js
+++ public/js/members.js
@@ -1,8 +1,4 @@
-// $(document).ready(function () {
-
- 
-
-    let localGifId = "";
+    let localGifUrl = "";
     let limit = 4;
     const apiKey = "W6nxutN5k5yRT98stgeJAxQjwXyesMTQ";
 
@@ -30,7 +26,7 @@
         console.log(gifImg);
 
         const gifTitle = response.data[i].title;
-        const gifId = response.data[i].id;
+        // const gifId = response.data[i].id;
 
 
         //set up for images and title data
@@ -39,7 +35,7 @@
         // const selectionLink = $("<a>").attr("href", "");
         const card = $("<div>").addClass("card col-sm-2");
         const imgTop = $("<div>").addClass("card-img-top");
-        const cardImg = $("<img>" + i).attr("src", gifImg);
+        const cardImg = $("<img>" + i).attr("src", gifImg);//saving the url
         const title = $("<p>").text(gifTitle);
 
         //creating top image card
@@ -56,7 +52,7 @@
         const selectedBtn = "gifSelection"+i;
         const cardButton = $("<button>")
           .addClass("btn btn-primary").attr("id", selectedBtn).text("Select");
-        cardButton.attr("data-id", gifId);
+        cardButton.attr("data-id", gifImg);
 
         //loading newpost.html to do the next action
         //this is where we can input comment
@@ -71,12 +67,13 @@
   
           
           //getting id on selected and next move. 
-          cardButton.on("click", function(gifId) {
-            debugger;
-             console.log(gifId); 
-            console.log("Value: ", gifId.currentTarget.attributes[2].value);
-            let newGifId = gifId.currentTarget.attributes[2].value;
-            localGifId = newGifId;
+          cardButton.on("click", function(gifUrl) {
+            
+             console.log(gifUrl); 
+            console.log("Value: ", gifUrl.currentTarget.attributes[2].value);
+            let newGifUrl = gifUrl.currentTarget.attributes[2].value;
+            // debugger;
+            localGifUrl = newGifUrl;
             //Call function that will load selected gif and control gif.html
             newPost();
 
@@ -85,32 +82,27 @@
       });
   }
   
-    console.log(localGifId);
+    console.log(localGifUrl);
 
-    //Function to control comment in gif.html
-
+    //loading newpost.html to do the next action
+    //this is where we can input comment
  function newPost(){
-    const queryUrl =
-    "https://api.giphy.com/v1/gifs/"+localGifId+"?api_key=" +
-        apiKey;
-  
-      $.ajax({
-        url: queryUrl,
-        method: "GET",
-      }).then(function (response) {
-        console.log(response)
-        
-        $.ajax({
+   
+         $.ajax({
           url:"/newpost",
           method:"POST",
           data: {
-            imagefile: response.data.images.original.url 
+            imagefile: localGifUrl//***********ERR****** */
           }
+        })
+        .then(function(err){
+          if (err) throw err;
+
         })
 // Do a POST request to server to send the selected Gif ID
 //Do a POST request to server to send input from comment box
         
-      })
+      // })
 
     }
 
