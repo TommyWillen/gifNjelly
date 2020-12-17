e7cfceb9e7e26b876996a48382e9a3eebe683fe8
CarolY <mon_alisa75@yahoo.com>
2020-12-17 14:57:48 -0800

    modify members.js

--- public/js/members.js
+++ public/js/members.js
@@ -1,117 +1,104 @@
-    let localGifUrl = "";
-    let limit = 4;
-    const apiKey = "W6nxutN5k5yRT98stgeJAxQjwXyesMTQ";
+let localGifUrl = "";
+let limit = 4;
+const apiKey = "W6nxutN5k5yRT98stgeJAxQjwXyesMTQ";
 
-  
-  //--------calling random gifs--------
+//--------calling random gifs--------
 
-  function randomGif() {
+function randomGif() {
+  const queryUrl =
+    "https://api.giphy.com/v1/gifs/trending?api_key=" +
+    apiKey +
+    "&limit=" +
+    limit +
+    "&rating=g";
 
-    const queryUrl =
-        "https://api.giphy.com/v1/gifs/trending?api_key=" +
-        apiKey +
-        "&limit=" +
-        limit +
-        "&rating=g";
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
 
-    $.ajax({
-      url: queryUrl,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
+    for (var i = 0; i < limit; i++) {
+      const gifImg = response.data[i].images.fixed_width.url;
+      console.log(gifImg);
 
-      for (var i = 0; i < limit; i++) {
+      const gifTitle = response.data[i].title;
+      // const gifId = response.data[i].id;
 
-        const gifImg = response.data[i].images.fixed_width.url;
-        console.log(gifImg);
+      //set up for images and title data
+      $("#img" + i).html("<img src=" + gifImg + ">");
 
-        const gifTitle = response.data[i].title;
-        // const gifId = response.data[i].id;
+      // const selectionLink = $("<a>").attr("href", "");
+      const card = $("<div>").addClass("card col-sm-2");
+      const imgTop = $("<div>").addClass("card-img-top");
+      const cardImg = $("<img>" + i).attr("src", gifImg); //saving the url
+      const title = $("<p>").text(gifTitle);
 
+      //creating top image card
+      imgTop.append(cardImg);
 
-        //set up for images and title data
-        $("#img" + i).html("<img src=" + gifImg + ">");
+      card.append(imgTop);
+      // card.append(title);
+      $("#imageCard").append(card, title);
 
-        // const selectionLink = $("<a>").attr("href", "");
-        const card = $("<div>").addClass("card col-sm-2");
-        const imgTop = $("<div>").addClass("card-img-top");
-        const cardImg = $("<img>" + i).attr("src", gifImg);//saving the url
-        const title = $("<p>").text(gifTitle);
+      const cardBody = $("<div>").addClass("card-body");
 
-        //creating top image card
-        imgTop.append(cardImg);
+      //individual id for each button
+      const selectedBtn = "gifSelection" + i;
+      const cardButton = $("<button>")
+        .addClass("btn btn-primary")
+        .attr("id", selectedBtn)
+        .text("Select");
+      cardButton.attr("data-id", gifImg);
 
-        card.append(imgTop);
-        // card.append(title);
-        $("#imageCard").append(card, title);
+      //loading newpost.html to do the next action
+      //this is where we can input comment
 
+      // selectionLink.append(cardButton);
+      cardBody.append(cardButton);
 
-        const cardBody = $("<div>").addClass("card-body");
+      //   cardBody.append(cardButton)
 
-        //individual id for each button
-        const selectedBtn = "gifSelection"+i;
-        const cardButton = $("<button>")
-          .addClass("btn btn-primary").attr("id", selectedBtn).text("Select");
-        cardButton.attr("data-id", gifImg);
+      //appending to main div
+      $("#imageCard").append(cardBody);
 
-        //loading newpost.html to do the next action
-        //this is where we can input comment
-
-        // selectionLink.append(cardButton);
-        cardBody.append(cardButton);
-
-        //   cardBody.append(cardButton)
-
-         //appending to main div
-          $("#imageCard").append(cardBody);
-  
-          
-          //getting id on selected and next move. 
-          cardButton.on("click", function(gifUrl) {
-            
-             console.log(gifUrl); 
-            console.log("Value: ", gifUrl.currentTarget.attributes[2].value);
-            let newGifUrl = gifUrl.currentTarget.attributes[2].value;
-            // debugger;
-            localGifUrl = newGifUrl;
-            //Call function that will load selected gif and control gif.html
-            newPost();
-
-          });
-        }
+      //getting id on selected and next move.
+      cardButton.on("click", function (gifUrl) {
+        console.log(gifUrl);
+        console.log("Value: ", gifUrl.currentTarget.attributes[2].value);
+        let newGifUrl = gifUrl.currentTarget.attributes[2].value;
+        // debugger;
+        localGifUrl = newGifUrl;
+        //Call function that will load selected gif and control gif.html
+        console.log("the url for localGifUrl:", localGifUrl);
+        newPost();
       });
-  }
-  
-    console.log(localGifUrl);
-
-    //loading newpost.html to do the next action
-    //this is where we can input comment
- function newPost(){
-   
-         $.ajax({
-          url:"/newpost",
-          method:"POST",
-          data: {
-            imagefile: localGifUrl//***********ERR****** */
-          }
-        })
-        .then(function(err){
-          if (err) throw err;
-
-        })
-// Do a POST request to server to send the selected Gif ID
-//Do a POST request to server to send input from comment box
-        
-      // })
-
     }
+  });
+}
 
-    //Click handler
-  $(document).ready(function(){
-    $("#searchButton").on("click", function () {
-      randomGif();
-      });
-  })
- 
-  
-  
+//loading newpost.html to do the next action
+//this is where we can input comment
+function newPost() {
+  $.ajax({
+    url: "/newpost",
+    method: "POST",
+    data: {
+      //***********ERR****** */
+      imagefile: localGifUrl,
+    },
+  }).then(function (err) {
+    console.log("checking localGifUrl value: ", localGifUrl);
+    
+    if (err) throw err;
+    
+    //need to redirect
+  });
+}
+
+//Click handler
+$(document).ready(function () {
+  $("#searchButton").on("click", function () {
+    randomGif();
+  });
+});
