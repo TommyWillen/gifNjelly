e94a9328dc0055c796d8f5bd062740832e0e32ee
CarolY <mon_alisa75@yahoo.com>
2020-12-16 19:11:33 -0800

    merge conflict resolution.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -18,15 +18,11 @@ app.get("/signup", function(req, res){
     res.render("signup");
 });
 
-<<<<<<< HEAD
-// app.get("/members", isAuthenticated, function(req, res) {
-//   res.render("/index");
-// });
-=======
+
+
 app.get("/members", function(req, res){
     res.render("members");
 });
->>>>>>> Frontend
 
 }
 // app.get("/members", isAuthenticated, function(req, res) {
