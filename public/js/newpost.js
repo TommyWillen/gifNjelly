650c74b30c027a25e326c22f2f1faebb62905d7a
aubrey-heim <aubrey.l.heim@gmail.com>
2020-12-17 21:30:01 -0800

    lintfix

--- public/js/newpost.js
+++ public/js/newpost.js
@@ -1,21 +1,18 @@
 $("#submitButton").on("click", function (event) {
-    event.preventDefault();
-    let gifId = $("#chosenGif").attr("data-id");
-    let caption = $("#caption-input").val();
-    let userId = $("#userId").val();
-    let giphyPost = {
-      caption: caption,
-      userId: userId
-    };
-    console.log(giphyPost);
-    $.ajax({
-      method: "POST",
-      url: "/api/giphypost/" + gifId,
-      data: giphyPost
-    }).then(
-      console.log(result)
-      // location.href = "/members"
-    ).catch(function (err) {
-      console.log(err);
-    });
+  event.preventDefault();
+  let gifId = $("#chosenGif").attr("data-id");
+  let caption = $("#caption-input").val();
+  let giphyPost = {
+    caption: caption,
+  };
+  console.log(giphyPost);
+  $.ajax({
+    method: "POST",
+    url: "/api/giphypost/" + gifId,
+    data: giphyPost
+  }).then(
+    console.log(result)
+  ).catch(function (err) {
+    console.log(err);
   });
+});
