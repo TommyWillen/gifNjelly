70708bd57ffd630e720507290c24d260008e50a7
aubrey-heim <aubrey.l.heim@gmail.com>
2020-12-17 20:05:35 -0800

    added oldgifs pages

---
+++ public/js/oldgifs.js
@@ -0,0 +1,33 @@
+function postVote(voteType, postId){
+  let gif;
+  let jelly;
+  if (voteType === "gif"){
+    gif = true;
+    jelly = false;
+  } else {
+    jelly = true;
+    gif = false;
+  }
+  let gifOrJelly = {
+    gif: gif,
+    jelly: jelly
+  };
+  $.ajax({
+    method: "POST",
+    url: "/api/vote/" + postId,
+    data: gifOrJelly
+  });
+}
+
+
+let postId;
+$(".gifVote").on("click", function (event){
+  event.preventDefault();
+  postId = $(this).attr("data-id");
+  postVote("gif", postId);
+});
+$(".jellyVote").on("click", function (event){
+  event.preventDefault();
+  postId = $(this).attr("data-id");
+  postVote("jelly", postId);
+});
